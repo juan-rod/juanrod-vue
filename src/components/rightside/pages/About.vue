5e15eddefe941035417aaab266b79242d3d82ea3
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2017-09-25 13:08:00 -0500

    final now version and about version

--- src/components/rightside/pages/About.vue
+++ src/components/rightside/pages/About.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
-    <div class="about-container">
-        <h3>about Page</h3>
+    <div class="about-container col-12">
+      <div class="row">
+        <div class="about-details col-12">
+          <p>I specialize in Front End Development</p>
+          <div>
+            <p>HTML5 | CSS3 | JavaScript</p>
+            <p>Vue.js | AngularJS 1.5 | Firebase</p>
+            <p>Sketch | Git </p>
+          </div>
+          <p> 
+            <span> adaptable.</span> 
+            <span> diligent.</span> 
+            <span> intuitive.</span>
+          </p>
+          <a href="mailto:juan.j.rodriguez2@gmail.com?Subject=Hey%20Juan" target="_top"><p class="link-box">Let's Chat! </p></a>
+        </div>
+        
+
+      </div>
         <!-- <span>rightside</span> -->
     </div>
   </div>
@@ -19,8 +36,49 @@ export default {
 
 <style scoped>
   .about-container{
-    background: white;
-    height:100%;
+    background: rgba(255, 255, 255, 1);
+    margin: 0 25px;
+    height: 450px;
+    font-family: 'Fira Mono', monospace;
+    box-shadow: 11px 13px 0px -2px rgb(113, 113, 113);
+    -webkit-box-shadow: 11px 13px 0px -2px rgb(113, 113, 113);
+      -moz-box-shadow: 11px 13px 0px -2px rgb(113, 113, 113);
+  }
+  .about-details{
+    text-align: right;
+    padding-top: 12%;
+    padding-right: 4%;
+    font-size: 1.1em;
+    font-weight: bold;
+  }
+  .about-details div{
+    font-size: .8em;
+    margin: 4% 0;
+  }
+  .about-details p span{
+    /*border:3px solid black;
+    border-style: inset;
+    padding:1%;*/
+  }
+  .about-details p span:hover{
+
+    /*border:1px solid blue;*/
+    text-shadow: 2px 2px 0px rgba(150, 150, 150, 1);
+    /*transition: all .5s ease-in;*/
+  }
+  .about-details a{
+    color: inherit;
+    text-decoration: none;
+  }
+  .about-details a p{
+    margin-top: 5%;
+
+  }
+   .about-details a:hover{
+    color: pink;
+  }
+  .link-box:hover{
+    background-color: #2c3e50;
   }
  
 </style>
--- src/components/rightside/pages/Now.vue
+++ src/components/rightside/pages/Now.vue
@@ -4,11 +4,17 @@
       <div class="row">
         <div class="now-details col-12">
           <p>Working on various personal projects</p>
-          <p>Side biz, Pride Austin Newsletter, PrideInTexas.com</p>
-          <p>Lovelyfaceuglycity.com is up! a creative outlet. Practicing ~*~design~*~.</p>
+          <p>Side biz, Pride Austin Newsletter, PrideInTexas</p>
+            <a href="http://lovelyfaceuglycity.com" target="_blank">
+          <p class="link-box"> 
+            <span>
+              Lovelyfaceuglycity.com
+            </span>
+          </p>
+            </a>
           <p>Joined the Austin Pride Board of Directors | Director of Technology &#x1F308</p>
           <p>Currently &#x1F4D6 | The Fall of Hyperion x Dan Simmons</p>
-          <p>Currently &#x1F3A7 | What Happened x Hillary Clinton.</p>
+          <p>Currently &#x1F3A7 | What Happened x Hillary Clinton</p>
           <br>
           <br>
           <small style="float: right">Updated Sept 16, 2017</small>
@@ -33,12 +39,13 @@ export default {
 
 <style scoped>
   .now-container{
-    background: rgba(255, 255, 255, 0.72);
+    background: rgba(255, 255, 255, 1);
     margin: 0 25px;
     height: 450px;
-    box-shadow: 6px 8px 3px -2px rgb(113, 113, 113);
-    -webkit-box-shadow: 6px 8px 3px -2px rgb(113, 113, 113);
-      -moz-box-shadow: 6px 8px 3px -2px rgb(113, 113, 113);
+    font-family: 'Fira Mono', monospace;
+    box-shadow: 11px 13px 0px -2px rgb(113, 113, 113);
+    -webkit-box-shadow: 11px 13px 0px -2px rgb(113, 113, 113);
+      -moz-box-shadow: 11px 13px 0px -2px rgb(113, 113, 113);
   }
   .now-details{
     text-align: right;
@@ -46,6 +53,16 @@ export default {
     padding-right: 4%;
     font-size: 1.1em;
     font-weight: bold;
+  }
+  .now-details a{
+    color: inherit;
+    text-decoration: none;
+  }
+   .now-details a:hover{
+    color: pink;
+  }
+  .link-box:hover{
+    background-color: #2c3e50;
   }
  
 </style>
